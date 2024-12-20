2bd76560ef9649146d1e96ed2a05a2aaea49a4b4
Sebinho <manredfan10@gmail.com>
2024-12-20 12:59:01 -0300

    Avance v1.0

--- app/app/jugadores/[id].jsx
+++ app/app/jugadores/[id].jsx
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, FlatList, Image, Dimensions } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import CountryFlag from "react-native-country-flag";
 
 const { width } = Dimensions.get('window'); 
 
@@ -48,10 +49,32 @@ export default function Jugador() {
         keyExtractor = {(item, index) => index.toString()}
         renderItem = {({ item }) => (
 
-          <View style = {styles.itemContainer}>
-            <Image style = {styles.imagenFondo} source = {{ uri: item.imagen2 }}/>
-          </View>
+          <View>
 
+            <View style = {styles.fondoContainer}>
+              <Image style = {styles.imagenFondo} source = {{ uri: item.imagen2 }}/>
+            </View>
+
+            <View style = {styles.infoContainer}>
+
+              <View style = {styles.infoContainer1}>
+                <Text style = {styles.edadTituloTexto}>Edad:</Text>
+                <Text style = {styles.edadTexto}>{item.edad}</Text>
+              </View>
+
+              <View style = {styles.infoContainer1}>
+                <Text style = {styles.edadTituloTexto}>Nacionalidad:</Text>
+                <Text style = {styles.edadTexto}>{item.nacionalidad}</Text>
+              </View>
+
+              <View style = {styles.infoContainer1}>
+                <Text style = {styles.edadTituloTexto}>Edad:</Text>
+                <Text style = {styles.edadTexto}>{item.edad}</Text>
+              </View>
+              
+            </View>
+            
+          </View>
         )}
       />
     </View>
@@ -61,12 +84,46 @@ export default function Jugador() {
 const styles = StyleSheet.create({
 
   body: { 
-    backgroundColor: '#353636' 
+    flex: 1,
+    backgroundColor: '#353636'
+  },
+
+  fondoContainer: {
+    flex: 1,
+    justifyContent: 'flex-end',
   },
 
   imagenFondo: {
     width: width,
-    height: 500
+    height: 300,
+    filter: 'grayscale(100%) brightness(79%)',
   },
+
+  infoContainer: {
+    alignItems: 'center',
+    textAlign: 'center',
+    backgroundColor: '#1c1e1e',
+    color: 'white',
+
+    margin: 25,
+    padding: 20,
+    borderRadius: 10
+  }, 
+
+  infoContainer1: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    textAlign: 'center',
+    backgroundColor: '#565656',
+    color: 'white',
+
+    padding: 10,
+    borderRadius: 10
+  },
+
+  edadTituloTexto: {
+    fontWeight: 'bold',
+    marginRight: 50
+  }
 
 });
